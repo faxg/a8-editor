cdaca64c8ac96ae5ed35d742333b83701789638e
Florian Georg <florian.alexander.georg@gmail.com>
2016-09-30 17:44:01 +0200

    Fixed for default A8 usage and A8_STANDALONE

--- services/webui/app/routes/index.js
+++ services/webui/app/routes/index.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var moment = require('moment');
 var request = require('request');
+var _ = require('underscore');
+var util = require('util');
+
 
 var router = express.Router();
+
+/* ok, this is a hack: if we run our services as standalone node apps for testing, the client UI directly calls out to the service
+  In a containerized environment, client calls back to the webui ('/proxy_to/<serviceName>/<endpoint>')
+  , which translates  we forward to the the sidecar (localhost:6379/<servicename>/<endpoint>), which proxies to the target service.
+  */
+
+var topologyServiceEndpoint = '/proxy_to/topologyservice/api';
+
 
 
 /* GET index page. */
@@ -12,18 +23,50 @@ router.get('/', function(req, res, next) {
     res.render('index', {
         lastRendered: timestamp.format('HH:mm:ss.SSS (x)'),
         services: {
-            'topologyService': 'http://localhost:3100/api',
+            'topologyService': topologyServiceEndpoint,
         },
         projectHomepage: 'http://github.io/a8-editor'
     })
 });
 
-/** Forward /api/<servicename>/<...>  */
-var apiUrl = 'http://localhost:6379';
-router.all('/api/*', function(req, res, next) {
-    var url = apiUrl + req.url.replace('/api', '');
-    console.log(url);
-    req.pipe(request(url)).pipe(res);
+
+
+/** 
+ * Forward /proxy_to/<servicename>/<...>  
+ * 
+ * 
+ * */
+var sideCarBase = 'http://localhost:6379';
+var standaloneTopologyServiceBase = 'http://localhost:3100';
+
+router.all('/proxy_to/:serviceName/:endpoint(*)', function(req, res, next) {
+    // construct url to service (standalone) or through sidecar proxy.   
+    var url = process.env.A8E_STANDALONE ?
+        standaloneTopologyServiceBase + '/' + req.params.endpoint :
+        sideCarBase + '/' + req.params.serviceName + '/' + req.params.endpoint;
+
+    // somewhat tricky way to re-construct the query string (if any)
+    var queryString = _.reduce(_.pairs(req.query), function(str, q, index, list) {
+        return (index == 0 ? '?' : '') + str + q[0] + "=" + encodeURIComponent(q[1]) + ((index < list.length - 1) ? '&' : '');
+    }, '');
+
+    url = url + queryString;
+    console.log('Proxying to microservice "' + req.params.serviceName + '"', url);
+    var forwardRequest = request(url);
+    req.pipe(forwardRequest).on('error', function(e) { handleError(e, url, forwardRequest, res, 'request') })
+        .pipe(res).on('error', function(e) { handleError(e, url, forwardRequest, res, 'response') });
+
+
 });
+
+function handleError(e, url, request, response, direction) {
+    console.log(e);
+    response.render('error', {
+        message: e,
+        url: url,
+        request: JSON.stringify(request, null, 2),
+        error: e
+    });
+}
 
 module.exports = router;
--- start-nocontainer.sh
+++ start-nocontainer.sh
@@ -3,14 +3,12 @@
 SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 pushd $SCRIPTDIR
-set PORT=3000
 cd $SCRIPTDIR/services/webui/app
-npm start &
+PORT=3000 A8E_STANDALONE=1 npm start &
 popd
 
 
 pushd $SCRIPTDIR
-set PORT=3100
 cd $SCRIPTDIR/services/topologyService/app
-npm start &
+PORT=3100 A8E_STANDALONE=1 npm start &
 popd
